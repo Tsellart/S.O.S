ff5f78808bbc3b5d7e45151eb8cb1c74780e1dbe
jlrt37 <joshua.l.ragon@gmail.com>
2019-01-30 19:29:43 -0600

    edited server.js, edited routes, added api.js in utils

--- my-app/Routes/index.js
+++ my-app/Routes/index.js
@@ -1,7 +1,11 @@
+const path = require("path");
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const bookRoutes = require("./api");
 
-// Book routes
-router.use("/books", bookRoutes);
+router.use("/api", apiRoutes);
+
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  });
 
 module.exports = router;
--- my-app/server.js
+++ my-app/server.js
@@ -1,24 +1,115 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var path = require("path");
+var axios = require("axios");
+var react = require("react")
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static("public"));
 
-// Define API routes here
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./src/components/pages/Form2.js"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+app.use(axios.json());
+app.use(axios.urlencoded({ extended: true }));
+
+app.use(react.json());
+app.use(react.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost/populate", {
+  useNewUrlParser: true
+});
+
+var db = mongojs(MONGODB_URI);
+
+db.on("error", function (error) {
+  console.log("Database Error:", error);
+});
+app.post("/submit", function (req, res) {
+  db.users.create(req.body)
+    .then(function (dbUsers) {
+      return db.Users.findOneAndUpdate({}, {
+        $push: {
+          users: dbUsers._id
+        }
+      }, {
+        new: true
+      });
+    })
+    .then(function (dbUsers) {
+      res.json(dbUsers);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.post("/submit", function (req, res) {
+  db.Items.create(req.body)
+    .then(function (dbItems) {
+      return db.Items.findOneAndUpdate({}, {
+        $push: {
+          items: dbItems._id
+        }
+      }, {
+        new: true
+      });
+    })
+    .then(function (dbItems) {
+      res.json(dbItems);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+
+app.get("/index", function (req, res) {
+  db.Items.find({})
+    .then(function (dbItems) {
+      res.json(dbItems);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+app.get("/index", function (req, res) {
+  db.Users.find({})
+    .then(function (dbUsers) {
+      res.json(dbUsers);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.get("/populated", function (req, res) {
+  db.Items.find({})
+    .populate("items")
+    .then(function (dbItems) {
+      res.json(dbItems);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
 });
